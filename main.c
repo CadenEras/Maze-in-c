13d4b7d6c93dba796848e049be49451a7f7e8396
Melissa Gries <gries.melissa@outlook.fr>
2022-07-02 23:40:09 +0200

    first maze generator

--- main.c
+++ main.c
@@ -1,106 +1,117 @@
-// C++ program to solve Rat in a Maze problem using
-// backtracking
-/*#include <stdio.h>
-#include <stdbool.h>
-// Maze size
-#define N 4
-
-bool solveMazeUtil(int maze[N][N], int x, int y,int sol[N][N]);
-
-// A utility function to print solution matrix sol[N][N]
-void printSolution(int sol[N][N])
-{
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++)
-            printf(" %d ", sol[i][j]);
-        printf("\n");
-    }
-}
-
-// A utility function to check if x, y is valid index for
-// N*N maze
-bool isSafe(int maze[N][N], int x, int y)
-{
-    // if (x, y outside maze) return false
-    if (x >= 0 && x < N && y >= 0 && y < N && maze[x][y] == 1)
-        return true;
-    return false;
-}
-
-// This function solves the Maze problem using Backtracking.
-// It mainly uses solveMazeUtil() to solve the problem. It
-// returns false if no path is possible, otherwise return
-// true and prints the path in the form of 1s. Please note
-// that there may be more than one solution, this function
-// prints one of the feasible solutions.
-bool solveMaze(int maze[N][N])
-{
-    int sol[N][N] = { { 0, 0, 0, 0 },
-                      { 0, 0, 0, 0 },
-                      { 0, 0, 0, 0 },
-                      { 0, 0, 0, 0 } };
-    if (solveMazeUtil(maze, 0, 0, sol) == false) {
-        printf("Solution doesn't exist");
-        return false;
-    }
-    printSolution(sol);
-    return true;
-}
-
-// A recursive utility function to solve Maze problem
-bool solveMazeUtil(int maze[N][N], int x, int y, int sol[N][N])
-{
-    // if (x, y is goal) return true
-    if (x == N - 1 && y == N - 1 && maze[x][y] == 1) {
-        sol[x][y] = 1;
-        return true;
-    }
-    // Check if maze[x][y] is valid
-    if (isSafe(maze, x, y) == true) {
-        // Check if the current block is already part of
-        // solution path.
-        if (sol[x][y] == 1)
-            return false;
-        // mark x, y as part of solution path
-        sol[x][y] = 1;
-         Move forward in x direction */
-        /*if (solveMazeUtil(maze, x + 1, y, sol) == true)
-            return true;
-        // If moving in x direction doesn't give solution
-        // then Move down in y direction
-        if (solveMazeUtil(maze, x, y + 1, sol) == true)
-            return true;
-        // If none of the above movements work then
-        // BACKTRACK: unmark x, y as part of solution path
-        sol[x][y] = 0;
-        return false;
-    }
-    return false;
-}*/
-
-// driver program to test above function
-/*int main()
-{
-    int maze[N][N] = { { 1, 0, 0, 0 },
-                       { 1, 1, 0, 1 },
-                       { 0, 1, 0, 0 },
-                       { 1, 1, 1, 1 } };
-    solveMaze(maze);
-    return 0;
-}*/
-
-// This code is contributed by Aditya Kumar (adityakumar129)*/
-
-
-//
 // Created by Melissa Gries (CadenEras) on 5/23/2022.
-//
-//Deep First Search algorithm
+#include "MovePlayer.h"
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <malloc.h>
+#include <windows.h>
 
-struct node {
+#define SCREEN_WIDTH    20
+#define SCREEN_HEIGHT   15
+#define START_X         0
+#define START_Y         7
+
+#define PLAYER '@'
+#define WALL '#'
+
+void CreateMaze(CHAR_INFO screenBuffer[], int playerX, int playerY);
+
+BOOL MovePlayer(CHAR_INFO screenBuffer[], COORD *playerPos);
+
+void main() {
+    CHAR_INFO screenBuffer[SCREEN_WIDTH * SCREEN_HEIGHT] = {0};
+    COORD bufferSize = {SCREEN_WIDTH, SCREEN_HEIGHT};
+    COORD playerPos = {START_X, START_Y};
+    int x = 0, y = 0;
+
+    srand(GetTickCount());
+
+    for (y = 0; y < bufferSize.Y; y++) {
+        for (x = 2; x < bufferSize.X; x++) {
+            if (!(rand() % 3)){
+                screenBuffer[x + y * SCREEN_WIDTH].Char.AsciiChar = WALL;
+                screenBuffer[x + y * SCREEN_HEIGHT].Attributes = FOREGROUND_GREEN;
+            }
+        }
+    }
+
+    CreateMaze(screenBuffer, playerPos.X, playerPos.Y);
+
+    while (1) {
+        if (MovePlayer(screenBuffer, &playerPos)) {
+            CreateMaze(screenBuffer, playerPos.X, playerPos.Y);
+        }
+    }
+}
+
+void CreateMaze(CHAR_INFO screenBuffer[], int playerX, int playerY) {
+    SMALL_RECT drawRect = {0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1};
+    COORD bufferSize = {SCREEN_WIDTH, SCREEN_HEIGHT};
+    COORD writeFrom = {0, 0};
+    HANDLE hOutput;
+
+    hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    screenBuffer[playerX + playerY * SCREEN_WIDTH].Char.AsciiChar = PLAYER;
+    screenBuffer[playerX + playerY * SCREEN_HEIGHT].Attributes = FOREGROUND_RED;
+
+    WriteConsoleOutput(hOutput, screenBuffer, bufferSize, writeFrom, &drawRect);
+}
+
+BOOL MovePlayer(CHAR_INFO screenBuffer[], COORD *playerPos) {
+    INPUT_RECORD InputRecord;
+    COORD oldPosition = *playerPos;
+    DWORD Events=0;
+    HANDLE hInput;
+    BOOL bPlayerMoved = FALSE;
+    int bKeyDown = 0;
+
+    hInput = GetStdHandle(STD_OUTPUT_HANDLE);
+    ReadConsoleInput(hInput, &InputRecord, 1, &Events);
+
+    bKeyDown = InputRecord.Event.KeyEvent.bKeyDown;
+
+    if(InputRecord.EventType == KEY_EVENT && bKeyDown) {
+        if (InputRecord.Event.KeyEvent.wVirtualKeyCode == VK_RIGHT) {
+            if (playerPos ->X < (SCREEN_WIDTH - 1)) {
+                playerPos->X++;
+                bPlayerMoved = TRUE;
+            }
+        } else if (InputRecord.Event.KeyEvent.wVirtualKeyCode == VK_LEFT) {
+            if (playerPos->X > 0) {
+                playerPos->X++;
+                bPlayerMoved = TRUE;
+            }
+        } else if (InputRecord.Event.KeyEvent.wVirtualKeyCode == VK_UP) {
+            if (playerPos->Y > 0) {
+                playerPos->Y--;
+                bPlayerMoved = TRUE;
+            }
+        } else if (InputRecord.Event.KeyEvent.wVirtualKeyCode == VK_DOWN) {
+            if (playerPos->Y < (SCREEN_HEIGHT - 1)) {
+                playerPos->Y++;
+                bPlayerMoved = TRUE;
+            }
+        } else if (InputRecord.Event.KeyEvent.wVirtualKeyCode == VK_ESCAPE) {
+            exit(0);
+        }
+    }
+
+    FlushConsoleInputBuffer(hInput);
+
+    if (bPlayerMoved) {
+        if (screenBuffer[playerPos->X + playerPos->Y * SCREEN_WIDTH].Char.AsciiChar != WALL) {
+            screenBuffer[oldPosition.X + oldPosition.Y * SCREEN_WIDTH].Char.AsciiChar = ' ';
+            screenBuffer[oldPosition.X + oldPosition.Y * SCREEN_HEIGHT].Attributes = 0;
+        } else {
+            *playerPos = oldPosition;
+        }
+        return TRUE;
+    }
+    return TRUE;
+}
+
+/*struct node {
     int vertex;
     struct node* next;
 };
@@ -192,4 +203,4 @@ int main() {
     DFS(graph, 2);
 
     return 0;
-}
+}*/
